e03e5509009bf9e275a1a574057c8d47974a7f4c
cabraile <andre.prze.filho@gmail.com>
2025-09-01 17:35:10 +0000

    feat: robin core

---
+++ src/robin_core/robin_core_cpp/include/robin_core_cpp/core.h
@@ -0,0 +1,15 @@
+#ifndef ROBIN_CORE_CPP_CORE_H
+#define ROBIN_CORE_CPP_CORE_H
+
+#include <eigen3/Eigen/Geometry>
+
+namespace robin_core
+{
+
+using Vector3    = Eigen::Vector3d;
+using Pose3      = Eigen::Isometry3d;
+using Quaternion = Eigen::Quaterniond;
+
+} // namespace robin_core
+
+#endif
