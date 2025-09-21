a8f4ffbacb3ec3257f828f8baf3231d6b1b7ce27
cabraile <andre.prze.filho@gmail.com>
2025-09-21 12:31:11 -0300

    feat: gpio motor interface

--- platform/motor_interface.py
+++ platform/motor_interface.py
@@ -1,23 +1,30 @@
 
 import yaml
 import time
+from enum import Enum
 from pathlib import Path
 from gpiozero import Motor
 
-WORKSPACE_DIR=Path(__file__).parent.parent 
-SETTINGS_PATH= WORKSPACE_DIR / "settings.yaml"
+class MotorId:
+    LEFT: int = 0
+    RIGHT: int = 1
 
-if __name__ =="__main__":
-    with open(SETTINGS_PATH, "r") as settings_file:
-        settings_dict = yaml.load_all(settings_file)
-    pinout_dict = settings_dict["pinout"]
-    motor_left = Motor(pinout_dict["MOTOR_LEFT_PIN_1"], pinout_dict["MOTOR_LEFT_PIN_2"])
-    motor_right = Motor(pinout_dict["MOTOR_RIGHT_PIN_1"], pinout_dict["MOTOR_RIGHT_PIN_2"])
+class MotorInterface:
 
-    for i in range(3):
-        motor_left.forward(1)
-        motor_right.forward(0)
-        time.sleep(2)
-        motor_left.forward(0)
-        motor_right.forward(1)
-        time.sleep(2)
+    def __init__(self, settings_path: Path):
+        with open(settings_path, "r") as settings_file:
+            settings_dict = yaml.load(settings_file, Loader=yaml.FullLoader)
+        pinout_dict = settings_dict["pinout"]
+        self.motors: list[Motor] = [None, None]
+        self.motors[MotorId.LEFT] = Motor(pinout_dict["MOTOR_LEFT_PIN_1"], pinout_dict["MOTOR_LEFT_PIN_2"])
+        self.motors[MotorId.RIGHT] = Motor(pinout_dict["MOTOR_RIGHT_PIN_1"], pinout_dict["MOTOR_RIGHT_PIN_2"])
+
+    def activate_by_id(self, side: int, intensity: float) -> None:
+        if intensity >= 0:
+            self.motors[side].forward(intensity)
+        else:
+            self.motors[side].backward(intensity)
+
+    def activate(self, intensity_left: float, intensity_right: float):
+        self.activate_by_id(MotorId.LEFT, intensity_left)
+        self.activate_by_id(MotorId.RIGHT, intensity_right)
