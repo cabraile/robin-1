2087ec8c0995fdac6a0409e54e5f4ac62cbb6c8b
cabraile <andre.prze.filho@gmail.com>
2025-09-28 18:40:18 -0300

    chore: stub

--- robin_py/perception/pose_estimator.py
+++ robin_py/perception/pose_estimator.py
@@ -5,5 +5,8 @@ class PoseEstimator:
     def __init__(self):
         pass
 
+    def _identify_base_board(self, image_frame: ImageStamped) -> list:
+        pass
+
     def process_image(self, image_frame: ImageStamped) -> dict:
         pass
